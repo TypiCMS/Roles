518486471c571521063a54ef19e61c844ed299c4
Samuel De Backer <sdebacker@gmail.com>
2016-04-09 13:45:04 +0200

    $module removed

--- src/resources/views/admin/index.blade.php
+++ src/resources/views/admin/index.blade.php
@@ -1,14 +1,12 @@
 @extends('core::admin.master')
 
-@section('title', trans($module.'::global.name'))
+@section('title', trans('roles::global.name'))
 
 @section('main')
 
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
-    <a href="{{ route('admin::create-'.str_singular($module)) }}" class="btn-add" title="@lang($module.'::global.New')">
-        <i class="fa fa-plus-circle"></i><span class="sr-only">@lang($module.'::global.New')</span>
-    </a>
+    @include('core::admin._button-create', ['module' => 'roles'])
 
     <h1>
         <span>@{{ models.length }} @choice('roles::global.roles', 2)</span>
@@ -38,7 +36,7 @@
                 <tr ng-repeat="model in displayedModels">
                     <td typi-btn-delete action="delete(model, model.name)"></td>
                     <td>
-                        @include('core::admin._button-edit')
+                        @include('core::admin._button-edit', ['module' => 'roles'])
                     </td>
                     <td>@{{ model.name }}</td>
                 </tr>
