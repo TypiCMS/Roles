47522409b1c937cb1da9e4e08996c570e60275a4
Samuel De Backer <sdebacker@gmail.com>
2016-07-05 10:27:11 +0200

    sidebar item id

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -13,6 +13,7 @@ class SidebarViewComposer
     {
         $view->sidebar->group(trans('global.menus.users'), function (SidebarGroup $role) {
             $role->addItem(trans('roles::global.name'), function (SidebarItem $item) {
+                $item->id = 'roles';
                 $item->icon = config('typicms.roles.sidebar.icon', 'icon fa fa-fw fa-users');
                 $item->weight = config('typicms.roles.sidebar.weight');
                 $item->route('admin::index-roles');
