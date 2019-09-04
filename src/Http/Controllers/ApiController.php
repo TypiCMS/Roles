0ddd6a83a44eba2a65b4f8d2633b356ccba252ae
Samuel De Backer <sdebacker@gmail.com>
2019-09-04 22:07:45 +0200

    QueryBuilder upgrade

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -5,7 +5,7 @@ namespace TypiCMS\Modules\Roles\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Spatie\QueryBuilder\Filter;
+use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 use TypiCMS\Modules\Core\Filters\FilterOr;
 use TypiCMS\Modules\Core\Http\Controllers\BaseApiController;
@@ -16,8 +16,9 @@ class ApiController extends BaseApiController
     public function index(Request $request): LengthAwarePaginator
     {
         $data = QueryBuilder::for(Role::class)
+            ->allowedSorts(['name'])
             ->allowedFilters([
-                Filter::custom('name', FilterOr::class),
+                AllowedFilter::custom('name', new FilterOr),
             ])
             ->paginate($request->input('per_page'));
 
