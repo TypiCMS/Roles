62008005d5d99f609ad1b12a1e641a5a9e62e736
Samuel De Backer <sdebacker@gmail.com>
2020-09-17 23:51:46 +0200

    Callable syntax for controller route declarations.

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -5,18 +5,11 @@ namespace TypiCMS\Modules\Roles\Providers;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
+use TypiCMS\Modules\Roles\Http\Controllers\AdminController;
+use TypiCMS\Modules\Roles\Http\Controllers\ApiController;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to the controller routes in your routes file.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'TypiCMS\Modules\Roles\Http\Controllers';
-
     /**
      * Define the routes for the application.
      */
@@ -27,11 +20,11 @@ class RouteServiceProvider extends ServiceProvider
              * Admin routes
              */
             $router->middleware('admin')->prefix('admin')->group(function (Router $router) {
-                $router->get('roles', 'AdminController@index')->name('admin::index-roles')->middleware('can:read roles');
-                $router->get('roles/create', 'AdminController@create')->name('admin::create-role')->middleware('can:create roles');
-                $router->get('roles/{role}/edit', 'AdminController@edit')->name('admin::edit-role')->middleware('can:update roles');
-                $router->post('roles', 'AdminController@store')->name('admin::store-role')->middleware('can:create roles');
-                $router->put('roles/{role}', 'AdminController@update')->name('admin::update-role')->middleware('can:update roles');
+                $router->get('roles', [AdminController::class, 'index'])->name('admin::index-roles')->middleware('can:read roles');
+                $router->get('roles/create', [AdminController::class, 'create'])->name('admin::create-role')->middleware('can:create roles');
+                $router->get('roles/{role}/edit', [AdminController::class, 'edit'])->name('admin::edit-role')->middleware('can:update roles');
+                $router->post('roles', [AdminController::class, 'store'])->name('admin::store-role')->middleware('can:create roles');
+                $router->put('roles/{role}', [AdminController::class, 'update'])->name('admin::update-role')->middleware('can:update roles');
             });
 
             /*
@@ -39,9 +32,9 @@ class RouteServiceProvider extends ServiceProvider
              */
             $router->middleware('api')->prefix('api')->group(function (Router $router) {
                 $router->middleware('auth:api')->group(function (Router $router) {
-                    $router->get('roles', 'ApiController@index')->middleware('can:read roles');
-                    $router->patch('roles/{role}', 'ApiController@updatePartial')->middleware('can:update roles');
-                    $router->delete('roles/{role}', 'ApiController@destroy')->middleware('can:delete roles');
+                    $router->get('roles', [ApiController::class, 'index'])->middleware('can:read roles');
+                    $router->patch('roles/{role}', [ApiController::class, 'updatePartial'])->middleware('can:update roles');
+                    $router->delete('roles/{role}', [ApiController::class, 'destroy'])->middleware('can:delete roles');
                 });
             });
         });
