adeb80bc1f91514e825fb3be7e74934793958439
Samuel De Backer <sdebacker@gmail.com>
2021-02-08 18:02:21 +0100

    docblock removed

--- src/Presenters/ModulePresenter.php
+++ src/Presenters/ModulePresenter.php
@@ -6,9 +6,6 @@ use TypiCMS\Modules\Core\Presenters\Presenter;
 
 class ModulePresenter extends Presenter
 {
-    /**
-     * Get title.
-     */
     public function title(): string
     {
         return $this->entity->name;
