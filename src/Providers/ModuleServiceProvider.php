a97149109266e03e04347677a1c5848e3d07488e
Samuel De Backer <sdebacker@gmail.com>
2021-03-10 15:47:53 +0100

    Files moved

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -16,16 +16,16 @@ class ModuleServiceProvider extends ServiceProvider
         $modules = $this->app['config']['typicms']['modules'];
         $this->app['config']->set('typicms.modules', array_merge(['roles' => []], $modules));
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views/', 'roles');
+        $this->loadViewsFrom(__DIR__.'/../../resources/views/', 'roles');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/roles'),
+            __DIR__.'/../../resources/views' => resource_path('views/vendor/roles'),
         ], 'views');
 
         $this->publishes([
-            __DIR__.'/../database/seeders/RoleSeeder.php' => database_path('seeders/RoleSeeder.php'),
-            __DIR__.'/../database/seeders/PermissionSeeder.php' => database_path('seeders/PermissionSeeder.php'),
-            __DIR__.'/../database/seeders/PermissionRoleSeeder.php' => database_path('seeders/PermissionRoleSeeder.php'),
+            __DIR__.'/../../database/seeders/RoleSeeder.php' => database_path('seeders/RoleSeeder.php'),
+            __DIR__.'/../../database/seeders/PermissionSeeder.php' => database_path('seeders/PermissionSeeder.php'),
+            __DIR__.'/../../database/seeders/PermissionRoleSeeder.php' => database_path('seeders/PermissionRoleSeeder.php'),
         ], 'seeders');
 
         AliasLoader::getInstance()->alias('Roles', Roles::class);
